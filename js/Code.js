26344456569ce37b3df83c5d09af1099be56baa2
Arav08 <77580695+Arav08@users.noreply.github.com>
2021-05-16 12:05:59 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(12)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, \n not constant !!", 100,70)
+
+    fill("white")
+    textSize(12)
+    text("A T A D A S B A", 300, 50)
+    fill("lightblue")
+    text("Hint: Firbease Console !!", 300,70)
+
+    fill("white")
+    textSize(12)
+    text("C U T N I F O N", 500, 50)
+    fill("lightblue")
+    text("Group of statements that combine \n to form a task!!", 500,70)
+   
+
+}
